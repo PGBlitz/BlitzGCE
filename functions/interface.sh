2722de773b8b55b9e36eb13430026dfae71efcb0
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-02-04 01:29:50 -0500

    update

---
+++ functions/interface.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Title:      PlexGuide (Reference Title File)
+# Author(s):  Admin9705
+# URL:        https://plexguide.com - http://github.plexguide.com
+# GNU:        General Public License v3.0
+################################################################################
+source /opt/pggce/functions/main.sh
