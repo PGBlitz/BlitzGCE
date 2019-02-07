b5b6b846f7ed2e1209b6b44926f68afa374ea364
Admin9705 <user@dunn.cloud>
2019-02-07 14:30:17 -0500

    update

--- pggce.sh
+++ pggce.sh
@@ -57,6 +57,7 @@ case $typed in
         echo ""
         gcloud auth login --no-launch-browser --verbosity error --quiet
         echo "NOT-SET" > /var/plexguide/project.id
+        echo "TEST / TEST"
         ### note --no-user-output-enabled | gcloud auth login --enable-gdrive-access --brief
         # gcloud config configurations list
         gcestart ;;
