cb55952956f9af732c222b49ec0ad5d8d0a669fc
Admin9705 <user@dunn.cloud>
2019-02-06 19:49:14 -0500

    update

--- functions/main.sh
+++ functions/main.sh
@@ -27,7 +27,7 @@ variablepull () {
 
   #account=$(cat /var/plexguide/project.account)
   account=$(gcloud config configurations list | tail -n 1 | awk '{print $3}')
-  if [[ "$account" != "" ]]; then echo $account > /var/plexguide/project.account
+  if [[ "$account" != "" ]]; then echo $account > /var/plexguide/project.account; fi
 
   ipaddress=$(cat /var/plexguide/project.ipaddress)
   ipregion=$(cat /var/plexguide/project.ipregion)
